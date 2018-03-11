51e64a1a7e0710f8f045ed6edc6e8e0b965e99ae
dlh <dlhai@163.com>
2018-03-11 23:00:58 +0800

    blank

--- wood.py
+++ wood.py
@@ -91,6 +91,71 @@ base=tables["base"]
 base.sl = [base.c.title, base.c.name, base.c.forder, base.c.ftype, base.c.twidth, base.c.tstyle]
 organ = { "root": "winderco", "winderco": "winderprov", "winderprov": "winder", "winder":"winderarea","winderarea":"efan" }
 
+class User(UserMixin):
+    def __init__(self, username):
+        self.username = username
+        self.id = self.get_id()
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = password #generate_password_hash(password)
+
+    def verify_password(self, password):
+        #password_hash = self.get_password_hash()
+        #if password_hash is None:
+        #    return False
+        #return check_password_hash(self.password_hash, password)
+        return self.password == password:
+
+    def get_password_hash(self):
+        #try:
+        #    with open(PROFILE_FILE) as f:
+        #        user_profiles = json.load(f)
+        #        user_info = user_profiles.get(self.username, None)
+        #        if user_info is not None:
+        #            return user_info[0]
+        #except IOError:
+        #    return None
+        #except ValueError:
+        #    return None
+        #return None
+        return self.password
+
+    def get_id(self):
+        if self.username is not None:
+            try:
+                with open(PROFILE_FILE) as f:
+                    user_profiles = json.load(f)
+                    if self.username in user_profiles:
+                        return user_profiles[self.username][1]
+            except IOError:
+                pass
+            except ValueError:
+                pass
+        return unicode(uuid.uuid4())
+
+    @staticmethod
+    def get(user_id):
+        """try to return user_id corresponding User object.
+        This method is used by load_user callback function
+        """
+        if not user_id:
+            return None
+        try:
+            with open(PROFILE_FILE) as f:
+                user_profiles = json.load(f)
+                for user_name, profile in user_profiles.iteritems():
+                    if profile[1] == user_id:
+                        return User(user_name)
+        except:
+            return None
+        return None
+
+
 def QueryObj( sql ):
     result = conn.execute(sql).fetchall()
     ret = []
